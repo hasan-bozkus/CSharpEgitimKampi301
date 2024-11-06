efcf123eab60577b29928858bf4e33d21488dfc7
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-11-06 11:24:34 +0300

    C# Eğitim Kampı
    Ders 12 - OOP Modülü: Data Access Katmanı ve Context

--- CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -13,5 +13,8 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public int Productstock { get; set; }
         public decimal ProductPrice { get; set; }
         public string ProductDescription { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public List<Order> Orders { get; set; }
     }
 }
