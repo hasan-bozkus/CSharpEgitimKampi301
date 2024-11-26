4c1a29f9b02f74dd74632a3fb3abe174e2588910
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-11-26 14:18:23 +0300

    C# Eğitim Kampı
    Ders 18- EntityState Komutları, Generic Repository Sınıfı ve Ef Sınıfları

---
+++ CSharpEgitimKampi301.DataAccessLayer/EntityFramework/EFAdminDal.cs
@@ -0,0 +1,19 @@
+using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Repositories;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.EntityFramework
+{
+    public class EFAdminDal : GenericRepository<Admin>, IAdminDal
+    {
+    }
+}
+/*
+ Ekle, Sil, Güncelle, Listele ve Id'ye Göre Getir
+//Ýçinde A harfi geçmeyen kullanýcýlarý listele
+ */
